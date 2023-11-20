da1d03950110355770c5340e3a49d5c3cc8e0ddb
NalinduMelaka <nalindumelaka@outlook.com>
2023-11-20 16:06:14 +0530

    t

--- next.config.js
+++ next.config.js
@@ -2,10 +2,6 @@
 const nextConfig = {
   experimental: {
     serverActions: true
-  },
-  env: {
-    DATABASE_URL: process.env.DATABASE_URL,
-    NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET
   }
 }
 
